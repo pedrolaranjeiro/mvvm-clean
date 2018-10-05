d3ec95ddd6638dd95551d28c46d13d9c7aa3279e
Pedro Laranjeiro <pjalaranjeiro@gmail.com>
2018-10-05 12:26:36 +0100

    Add JUnit to classpath.

--- domain/src/test/java/uk/co/flat14/domain/usecase/articles/GetArticlesUseCaseTest.kt
+++ domain/src/test/java/uk/co/flat14/domain/usecase/articles/GetArticlesUseCaseTest.kt
@@ -4,7 +4,7 @@ import com.nhaarman.mockitokotlin2.*
 import io.reactivex.Single
 import org.junit.Test
 import uk.co.flat14.domain.usecase.exception.DataNotAvailableException
-import java
+import java.util.*
 
 class GetArticlesUseCaseTest {
 
